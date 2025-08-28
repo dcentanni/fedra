7e39da17c0bc9794e1dbc095194cc9ce89075d8a
dcentanni <daniele.centanni@outlook.it>
2025-08-28 17:16:22 +0200

    Test

--- src/appl/emrec/emvertex.cpp
+++ src/appl/emrec/emvertex.cpp
@@ -572,12 +572,12 @@ void DiscardImp(TEnv &env, EdbPVRec &v_vtx, float imp_max)
       vta = rfEVR.AddTrack(*vtx_new, (EdbTrackP*)v->GetTrack(t), v->GetVTa(t)->Zpos());
     }
     vtx_new->SetID(v->ID());
-    if (rfEVR.MakeV(*vtx_new)) {rfEVR.AddVertex(v);}
-    else {Log(1, "DiscardImp", "VERTEX %d NOT ADDED TO THE VERTEXREC", vtx_new->ID()); rfEVR.AddVertex(v);}
+    vtx_new->SetFlag(v->Flag());
+    if (rfEVR.MakeV(*vtx_new)) {rfEVR.AddVertex(vtx_new);}
+    else {Log(1, "DiscardImp", "VERTEX %d NOT ADDED TO THE VERTEXREC", vtx_new->ID());} //rfEVR.AddVertex(v);}
     Log(2, "DiscardImp", "New vertex created vID=%d, prob is %.3f, the original one was %.3f", vtx_new->ID(), vtx_new->V()->prob(), v->V()->prob());
     //v->Print();
     //vtx_new->Print();
-    vtx_new->SetFlag(v->Flag());
   }
 }
 //-----------------------------------------------------------------------------
